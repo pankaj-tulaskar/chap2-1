ea46ee7fd333b5d84ec904ca7f5bfa64d0988611
pankaj s tulaskar <pst28@afsconnect1.njit.edu>
2017-02-13 04:21:19 -0500

    used  array to get the product description, list price, and discount percentage from the form

--- display_discount.php
+++ display_discount.php
@@ -1,3 +1,8 @@
+<?php
+	$product_description = $_POST['product_description'];
+    	$list_price = $_POST['list_price'];
+        $discount_percent = $_POST['discount_percent'];
+?>
 <!DOCTYPE html>
 <html>
 <head>
