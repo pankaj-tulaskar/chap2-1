beb477b8e5a38351927dea4f65c07bdd42d7834a
pankaj s tulaskar <pst28@afsconnect1.njit.edu>
2017-02-13 04:38:19 -0500

    modified first three echo statements

--- display_discount.php
+++ display_discount.php
@@ -14,13 +14,13 @@
  <h1>This page is under construction</h1>
 
  <label>Product Description:</label>
-<span><?php echo ''; ?></span><br>
+<span><?php echo $product_description; ?></span><br>
 
  <label>List Price:</label>
- <span><?php echo ''; ?></span><br>
+ <span><?php echo $list_price; ?></span><br>
 
 <label>Standard Discount:</label>
-<span><?php echo ''; ?></span><br>
+<span><?php echo $discount_percent ; ?></span><br>
 
 <label>Discount Amount:</label>
 <span><?php echo ''; ?></span><br>
