c245bd99e36d39650b72ad4c4f835e11c73678e8
pankaj s tulaskar <pst28@afsconnect1.njit.edu>
2017-02-13 05:05:08 -0500

    added htmlspecialchars function

--- display_discount.php
+++ display_discount.php
@@ -23,13 +23,13 @@
  <h1>This page is under construction</h1>
 
  <label>Product Description:</label>
-<span><?php echo $product_description; ?></span><br>
+<span><?php echo htmlspecialchars($product_description); ?></span><br>
 
  <label>List Price:</label>
- <span><?php echo $list_price_f; ?></span><br>
+ <span><?php echo htmlspecialchars($list_price_f); ?></span><br>
 
 <label>Standard Discount:</label>
-<span><?php echo $discount_percent_f ; ?></span><br>
+<span><?php echo htmlspecialchars($discount_percent_f) ; ?></span><br>
 
 <label>Discount Amount:</label>
 <span><?php echo $discount_f; ?></span><br>
